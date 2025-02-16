0e889a505caac460ca67839b7e9c4f7c28ab3902
Amirhan15 <abdrahmanov_ob221bt@mail.ru>
2025-02-16 16:10:24 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './', // Убедись, что корень — текущая папка
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      input: './index.html' // Путь к index.html
+      input: './index.html',
+      external: ['/src/main.jsx'] // Добавляем сюда путь
     }
   }
 })
